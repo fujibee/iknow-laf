db21271bac2597fde2df6f18250d2a915196fa59
fujibee <fujibee@gmail.com>
2009-01-29 09:52:43 +0900

    show loading image while loading panels

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,6 +1,14 @@
 module GameHelper
+
   def iknow_link(item)
     link_to item.display_name, "http://www.iknow.co.jp/item/#{item.iknow_id}",
             :class => 'iknow_link', :target => "_blank"
   end
+
+  def loading_script
+    "$(first_item_panel).innerHTML = '" + image_tag('loading.gif') + "';" +
+    "$(last_item_panel).innerHTML = '" + image_tag('loading.gif') + "';" +
+    "document.body.style.cursor = 'wait';"
+  end
+
 end
